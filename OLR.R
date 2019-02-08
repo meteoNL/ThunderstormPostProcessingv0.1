0328f6da9f3324a52fa6a41ee687dfb89fb5e6cd
meteoNL <43781163+meteoNL@users.noreply.github.com>
2019-02-08 14:39:55 +0000

    Update OLR.R

--- OLR.R
+++ OLR.R
@@ -1,27 +1,18 @@
 ########################################
-#This is without any transformations!!!#
-#For now, sample climatology is used!!!#
-########################################
-
-
-#######
-
-
-#THIS IS STILL USING COMPLEMENTARY PROBABILITIES P_y=0 = (1-P_y=1)
-
-#######
+#This model type was used to assess the linearity of transformed threshold with power (0.25)
 
 ### LOAD EXTERNAL CODE
 library(dplyr)
 library(MASS)
 library(verification)
+library(ordinal)
 library(arm)
 library(crch)
 library(profvis)
 #rm(list=ls(all=TRUE))
 ### SET GENERAL CONDITIONS FOR THE MODEL
 #set thresholds and hyperparameter; determine test dataset and training dataset
-p = 0.5 #power transformation to linearize thresholds
+p = 0.25 #power transformation to linearize thresholds
 maxvars = 4
 numsubset = 3 #number of subsets for hyperparameter selection
 thres = seq(5,70,5)
@@ -36,11 +27,6 @@ regions = c(unique(ObsPV$region))#[1:2]
 climset = filter(ObsPV, Ndischarge > minpredictant & validtime.x == VT & leadtime_count == LT)
 predictant = 113
 
-#do transformations for thresholds
-#climset$Dischargerate = climset$Dischargerate^p
-#thres = thres^p
-#thres_eval = thres_eval^p
-
 #selects the best predictor by forward fittng, trying potential predictors and selecting the one with lowest AIC
 observed = seq(length(climset))*0
 for(i in seq(length(thres))){
@@ -50,5 +36,5 @@ for(i in seq(length(thres))){
 }
 
 model = clm(as.factor(observed)~ .,
-               data=data.frame(climset[32]), link = "logit", start = c((thres^0.5),-0.05))
-plot(model$coefficients[1:(length(model$coefficients)-1)], thres^0.5)
+               data=data.frame(climset[32]), link = "logit", start = c((thres^p),-0.05))
+plot(model$coefficients[1:(length(model$coefficients)-1)], thres^p)
