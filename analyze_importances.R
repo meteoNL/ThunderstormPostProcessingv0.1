f19ab30df377fe031dcd7b6c546ba6f3cb950e88
meteoNL <43781163+meteoNL@users.noreply.github.com>
2019-02-18 09:27:58 +0000

    Update analyze_importances.R

--- analyze_importances.R
+++ analyze_importances.R
@@ -1,13 +1,18 @@
+# read data from file
 setwd("/usr/people/groote/ThunderstormPostProcessingv1/rangerres")
 data = read.csv("ECimp.csv")
 
-#select certain hyperparameter optionally:
+# select certain hyperparameter optionally (and if so, switch potentially commented filter on):
 mtries = 2
 nsize = 3
-#data <- filter(data, mtry == mtries, min_n_size == nsize)
+# data <- filter(data, mtry == mtries, min_n_size == nsize)
 maxpredictors = 10
 data = filter(data, npred < (maxpredictors + 0.5))
+
+# generate table with relative frequencies of the data table; plot it
 freqtable=prop.table(table(data$varnames))
 plot(sort(as.numeric(freqtable),decreasing = TRUE))
+
+#sort and write relevant part of the table with predictors that occur
 freqtable=sort(freqtable[freqtable > 0],decreasing = TRUE)
 write.csv(freqtable, file = "EC_case_LT_3_VT_1218.csv")
